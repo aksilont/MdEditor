49f8827918c373044071d9bded7c2162f0e09e50
aksilont <aksilont@gmail.com>
2023-12-25 00:29:05 +0300

    Aded GitHub Action

---
+++ .github/build.sh
@@ -0,0 +1,8 @@
+tuist fetch
+tuist generate
+xcodebuild clean -quiet
+xcodebuild test \
+    -workspace 'MdEditor.xcworkspace' \
+    -scheme 'MdEditor' \
+    -destination 'platform=iOS Simulator,name=iPhone 14 Pro' \
+    test
