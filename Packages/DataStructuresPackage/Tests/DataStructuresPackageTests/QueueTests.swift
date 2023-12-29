80bca2f997872a029bec2022ca0fb142e0ca957c
Konstantin Abroskin <64919648+aksilont@users.noreply.github.com>
2023-12-29 12:44:14 +0300

    Mded 4 spm data structures package (#2)
    * MDED-2 Добавление TaskManagerPackage в репозиторий
    
    * MDED-4 Добавление DataStructuresPackage в репозиторий

---
+++ Packages/DataStructuresPackage/Tests/DataStructuresPackageTests/QueueTests.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Константин Натаров on 27.12.2023.
+//
+
+import XCTest
+@testable import DataStructuresPackage
+
+final class QueueTests: XCTestCase {
+	/// testing here
+}
+
