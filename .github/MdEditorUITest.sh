21eefaf73711ce0bf8fbec1b47c9e241ff23f80d
aksilont <aksilont@gmail.com>
2024-01-25 15:52:53 +0300

    CI for UI tests

---
+++ .github/MdEditorUITest.sh
@@ -0,0 +1,8 @@
+cd App/
+tuist generate
+xcodebuild clean test \
+    -quiet \
+    -workspace 'MdEditor.xcworkspace' \
+    -scheme 'MdEditorUITests' \
+    -sdk iphonesimulator \
+    -destination 'platform=iOS Simulator,name=iPhone 14 Pro'
--- .github/build.sh
+++
@@ -1,7 +0,0 @@
-tuist generate -p App
-xcodebuild clean -quiet
-xcodebuild test \
-    -workspace 'App/MdEditor.xcworkspace' \
-    -scheme 'MdEditor' \
-    -destination 'platform=iOS Simulator,name=iPhone 14 Pro' \
-    test
